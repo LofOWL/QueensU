ed718d1c897e5a859463ef3b50676b9201737636
DongJun Jin <jeffersonjjin@gmail.com>
2019-09-07 09:48:02 -0400

    change index createData detailsPage

--- miniprogram/mainpages/createData/createData.js
+++ miniprogram/mainpages/createData/createData.js
@@ -218,6 +218,7 @@ Page({
       var month = date.getMonth() + 1
       var day = date.getDate()
       this.setData({
+        date: [year,month,day].map(formatNumber).join('-'),
         createDate: [year,month,day].map(formatNumber).join('-'),
         endDate: [endyear,month,day].map(formatNumber).join('-'),
       })
